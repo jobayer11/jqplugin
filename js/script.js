cdf6a1bb53d7654eafb4824cec97b42fbb1ce7a1
Md Jobayer Hossain <65123276+jobayerhossain4@users.noreply.github.com>
2020-09-09 04:04:52 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,65 @@
+  $(document).ready(function(){
+    // slick slider start
+    $('.autoplay').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows:false,
+      dots: true
+    });
+    // slick slider end
+    // counterup start
+    $('.counter').counterUp({
+      delay: 10,
+      time: 1000
+    });
+    // counterup end
+    // flipping gallery start
+    $(".gallery").flipping_gallery({
+      direction: "forward", // This is will set the flipping direction when the gallery is clicked. Options available are "forward", or "backward". The default value is forward.
+      selector: "> a", // This will let you change the default selector which by default, will look for <a> tag and generate the gallery from it. This option accepts normal CSS selectors.
+      spacing: 10, // You can set the spacing between each photo in the gallery here. The number represents the pixels between each photos. The default value is 10.
+      showMaximum: 15, // This will let you limit the number of photos that will be in the viewport. In case you have a gazillion photos, this is perfect to hide all those photos and limit only a few in the viewport.
+      enableScroll: true, // Set this to false if you don't want the plugin to override your scrolling behavior. The default value is true.
+      flipDirection: "bottom", // You can now set which direction the picture will flip to. Available options are "left", "right", "top", and "bottom". The default value is bottom.
+      autoplay: false // You can set the gallery to autoplay by defining the interval here. This option accepts value in milliseconds. The default value is false.
+    });
+    // flapping gallery end
+    // event calendar start
+        $("#calendar").simpleCalendar({
+
+          // displays events
+          displayEvent: true,
+
+          // event dates
+          events: [
+            // generate new event after tomorrow for one hour
+            {
+              startDate: new Date(new Date().setHours(new Date().getHours() + 24)).toDateString(),
+              endDate: new Date(new Date().setHours(new Date().getHours() + 25)).toISOString(),
+              summary: 'Visit of the Eiffel Tower'
+            },
+            // generate new event for yesterday at noon
+            {
+              startDate: new Date(new Date().setHours(new Date().getHours() - new Date().getHours() - 12, 0)).toISOString(),
+              endDate: new Date(new Date().setHours(new Date().getHours() - new Date().getHours() - 11)).getTime(),
+              summary: 'Restaurant'
+            },
+            // generate new event for the last two days
+            {
+              startDate: new Date(new Date().setHours(new Date().getHours() - 48)).toISOString(),
+              endDate: new Date(new Date().setHours(new Date().getHours() - 24)).getTime(),
+              summary: 'Visit of the Louvre'
+            }
+          ],
+
+          // disable showing event details
+          disableEventDetails: false,
+
+          // disable showing empty date details
+          disableEmptyDetails: false 
+
+        });
+    // event calendar end
+});
